5d719a696b286469709d5b6099838447efcc1ac4
Stino <Linksonder@gmail.com>
2014-12-05 10:11:19 +0100

    Initial

---
+++ MvcApiJsonDemo/Controllers/Api/ModuleController.cs
@@ -0,0 +1,46 @@
+﻿using MvcApiJsonDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApiJsonDemo.Controllers.Api
+{
+    public class ModuleController : ApiController
+    {
+        private List<Module> modules = new List<Module>()
+            {
+                new Module{Id = 0, Naam = "test module a" },
+                 new Module{Id = 1, Naam = "test module b" },
+            };
+
+        // GET api/<controller>
+        public IEnumerable<Module> Get()
+        {
+            return modules;
+        }
+
+        // GET api/<controller>/5
+        public Module Get(int id)
+        {
+            return modules.SingleOrDefault(m => m.Id == id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MvcApiJsonDemo/Models/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApiJsonDemo.Models
+{
+    public class Module
+    {
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+    }
+}
